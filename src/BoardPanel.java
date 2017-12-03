180387248e454f0641d3fea2aab4104bfb4bd4fb
PC 16 <PC 16@Cyber-107Pc16>
2017-12-03 23:52:50 +0200

    commit 2

--- src/BoardPanel.java
+++ src/BoardPanel.java
@@ -7,18 +7,30 @@ public class BoardPanel extends JPanel {
 	private Square _mat[];
 private Img _img1;
 	public BoardPanel (){
+		int i;
 		  _mat = new Square[61];		
 		 _img1 = new Img("images\\rollsback.png", 0, 0, 2000, 1000);
-		  for(int i = 0;i<61;i++)
+		  for( i = 0;i<5;i++)
 			{
 				_mat[i] = new Square(i, 0, new Img("images\\blackBall.png", 0, 0, 100, 100), new Img("Images\\blackBall.png", 20, 20, 50, 50));
 				setLayout(null);
-				 _mat[i].setBounds(630 + i*150, 150 + i, 100, 100);
+				
+				 _mat[i].setBounds(655 + i*150, 160 + i, 70, 70);
+				  add(_mat[i]);
+				  
+				}
+		  for( i = 0;i<6;i++)
+			{
+				_mat[i] = new Square(i, 0, new Img("images\\blackBall.png", 0, 0, 100, 100), new Img("Images\\blackBall.png", 20, 20, 50, 50));
+				setLayout(null);
+				
+				 _mat[i].setBounds(580 + i*150, 238 + i, 70, 70);
 				  add(_mat[i]);
 				  
 				}
 			
 			}	  
+	
 	public void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
